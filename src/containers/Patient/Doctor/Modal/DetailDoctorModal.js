3479f45488c02393cbeba4900a0489b39223de1c
vuongoctien <vuongoctien@gmail.com>
2023-10-18 13:57:03 +0700

    hmm lằng nhằng ra phết

--- src/containers/Patient/Doctor/Modal/DetailDoctorModal.js
+++ src/containers/Patient/Doctor/Modal/DetailDoctorModal.js
@@ -28,9 +28,7 @@ class DetailDoctorModal extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            view_or_edit: true,
-            imageBase64: '',
-            previewImgURL: '',
+
         }
     }
 
@@ -38,20 +36,6 @@ class DetailDoctorModal extends Component {
     }
 
     async componentDidUpdate(prevProps, prevState, snapshot) { }
-
-    handleOnChangeImage = async (event) => {
-        let data = event.target.files
-        let file = data[0]
-        if (file) {
-            let base64 = await CommonUtils.getBase64(file)
-            let objectUrl = URL.createObjectURL(file)
-            this.setState({
-                previewImgURL: objectUrl,
-                imageBase64: base64
-            })
-        }
-        document.getElementById('kec').innerHTML = ''
-    }
 
     render() {
         console.log('this.props.infoDoctor', this.props.infoDoctor)
@@ -67,65 +51,27 @@ class DetailDoctorModal extends Component {
                 <div className='booking-modal-content'>
                     <div className='booking-modal-header'>
                         <span className='left'>Đây là DetailDoctorModal</span>
-                        <span className='right' onClick={() => {
-                            this.setState({ view_or_edit: true })
-                            this.props.closeBookingClose()
-                        }}>
-                            <i className='fas fa-times'></i>
-                        </span>
+                        <span className='right' onClick={() => { this.props.closeBookingClose() }}><i className='fas fa-times'></i></span>
                     </div>
 
                     <div className='booking-modal-body'>
                         <div className='col-12 row'>
                             <div className='col-3'>
-                                {this.state.view_or_edit === true ?
-                                    <div
-                                        className='col-12 doctor-image-modal'
-                                        style={{ backgroundImage: `url(${this.props.infoDoctor.image})` }}
-                                    // style={{ backgroundImage: `url(${this.state.previewImgURL})` }}
-                                    ></div>
-                                    :
-                                    <div
-                                        className='col-12 doctor-image-modal' id='kec'
-                                        // style={{ backgroundImage: `url(${this.props.infoDoctor.image})` }}
-                                        style={{ backgroundImage: `url(${this.state.previewImgURL})` }}
-                                    >
-                                        {/* <h1><i className="fas fa-user-tie"></i></h1> */}
-                                        <img id='old-img-edit' src={this.props.infoDoctor.image} />
-                                    </div>
-                                }
-                                <div className='col-12'>
-                                    {this.state.view_or_edit === true ?
-                                        <>
-                                            <br />
-                                            <label id='label-upload' style={{ opacity: 0 }}>Chọn ảnh <i class="fas fa-images"></i></label>
-                                        </>
-                                        :
-                                        <>
-                                            <input onChange={(event) => this.handleOnChangeImage(event)} className='form-control' type='file' id='default_button' hidden />
-                                            <br />
-                                            <label id='label-upload' htmlFor='default_button'>Chọn ảnh <i class="fas fa-images"></i></label>
-                                        </>
-                                    }
-                                </div>
+                                <div
+                                    className='col-12 doctor-image-modal'
+                                    style={{ backgroundImage: `url(${this.props.infoDoctor.image})` }}
+                                ></div>
                             </div>
 
 
                             <div className='col-9 row'>
                                 <div className='col-6'>
                                     <div><h6>Họ và tên: </h6></div>
-                                    {this.state.view_or_edit === true ?
-                                        <div><input disabled className='form-control' type="text" value={this.props.infoDoctor.name} /></div>
-                                        :
-                                        <div><input className='form-control' type="text" value={this.props.infoDoctor.name} /></div>
-                                    }
+                                    <div><input disabled className='form-control' type="text" value={this.props.infoDoctor.name} /></div>
                                 </div>
                                 <div className='col-6'>
                                     <div><h6>Chức danh: </h6></div>
-                                    {this.state.view_or_edit === true ?
-                                        <div><input disabled className='form-control' type="text" value={this.props.infoDoctor.position} /></div>
-                                        :
-                                        <div><input className='form-control' type="text" value={this.props.infoDoctor.position} /></div>}
+                                    <div><input disabled className='form-control' type="text" value={this.props.infoDoctor.position} /></div>
                                 </div>
                                 <div className='col-12'><br /></div>
                                 <div className='col-6'>
@@ -141,20 +87,9 @@ class DetailDoctorModal extends Component {
                                     <div><h6><br /></h6></div>
 
                                     <div>
-                                        {
-                                            this.state.view_or_edit === true ?
-                                                <button type='button' className='btn btn-info'
-                                                    onClick={() => { this.setState({ view_or_edit: false }) }}>Chỉnh sửa thông tin <i className="far fa-edit"></i>
-                                                </button>
-                                                :
-                                                <button type='button' className='btn btn-secondary'
-                                                    onClick={() => { if (window.confirm(`Bạn muốn thôi chỉnh sửa?`) === true) { window.location.reload() } }}
-                                                >
-                                                    Hủy thay đổi
-                                                    <i className="fas fa-window-close"></i>
-                                                </button>
-                                        }
-
+                                        <button type='button' className='btn btn-info'
+                                            onClick={() => { }}>Chỉnh sửa thông tin <i className="far fa-edit"></i>
+                                        </button>
                                     </div>
                                 </div>
                             </div>
@@ -177,15 +112,7 @@ class DetailDoctorModal extends Component {
                         </div>
                         <div className='col-12'><br /></div>
                         <div style={{ minHeight: '70vh' }}>
-                            {this.state.view_or_edit === true ?
-                                <>{this.props.infoDoctor.descriptionHTML}</>
-                                :
-                                <MdEditor
-                                    style={{ height: '100vh' }}
-                                    renderHTML={text => mdParser.render(text)}
-                                    value={this.props.infoDoctor.descriptionMarkdown}
-                                />}
-
+                            {this.props.infoDoctor.descriptionHTML}
                         </div>
                     </div>
 
