b49ab8068ebe187275212cb82f2d67a810e3d435
vuongoctien <vuongoctien@gmail.com>
2023-11-07 19:36:06 +0700

    xong hiển thị lịch trang quản trị

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -10,6 +10,7 @@ import FooterClinic from '../Footer/FooterClinic';
 import DatePicker from 'react-flatpickr';
 import { getOrderByDate, getAllDoctorByClinicId, getAllMediPackageByClinicId } from '../../services/userService';
 import moment from 'moment/moment';
+import { Modal } from 'reactstrap';
 
 class UserManage extends Component {
     constructor(props) {
@@ -17,7 +18,8 @@ class UserManage extends Component {
         this.state = {
             datePicked: new Date(),
             arrOrder: [],
-
+            isOpenDetailPatient: false,
+            thisPatient: {}
         }
     }
 
@@ -51,12 +53,70 @@ class UserManage extends Component {
         this.fetchAllOrderByDate()
     }
 
+    themSo_0 = (number) => {
+        if (number < 10) number = '0' + number
+        return number
+    }
+
     render() {
         console.log('this.state', this.state)
         console.log('this.props', this.props)
         return (
 
             <div className='dondatlich'>
+                <Modal
+                    isOpen={this.state.isOpenDetailPatient}
+                    thisPatient={this.state.thisPatient}
+                    className={'booking-modal-container'}
+                    size='lg'
+                    centered
+                >
+                    <div className='head2'>
+                        <h5><b>Thông tin bệnh nhân: </b></h5>
+                        <div><i onClick={() => { this.setState({ isOpenDetailPatient: false }) }}
+                            class="far fa-window-close fa-lg"></i></div>
+                    </div>
+                    <div className='thongbaodatlichthanhcong'>
+
+                        <div>
+                            <table>
+                                <tr><td><u><i><b>Thông tin người đặt lịch: </b></i></u></td></tr>
+                                <tr>
+                                    <td className='td1'>Số điện thoại: &ensp;</td>
+                                    <td className='td2'>{this.state.thisPatient.phoneNumber}</td>
+                                </tr>
+                                <tr>
+                                    <td className='td1'>Email: &ensp;</td>
+                                    <td className='td2'>{this.state.thisPatient.email}</td>
+                                </tr>
+                                <tr>
+                                    <td className='td1'>Đặt cho mình hay cho người thân? &ensp;</td>
+                                    <td className='td2'>{this.state.thisPatient.forwho === '1' ? 'Đặt cho mình' : 'Đặt cho người thân'}</td>
+                                </tr>
+                                <tr><td><u><i><b>Thông tin bệnh nhân: </b></i></u></td></tr>
+                                <tr>
+                                    <td className='td1'>Họ và tên: &ensp;</td>
+                                    <td className='td2'>{this.state.thisPatient.patientName}</td>
+                                </tr>
+                                <tr>
+                                    <td className='td1'>Ngày/tháng/năm sinh: &ensp;</td>
+                                    <td className='td2'>
+                                        {moment(this.state.thisPatient.patientBirthday)._d.getDate()}/
+                                        {+moment(this.state.thisPatient.patientBirthday)._d.getMonth() + 1}/
+                                        {moment(this.state.thisPatient.patientBirthday)._d.getFullYear()}
+                                    </td>
+
+                                </tr>
+                                <tr>
+                                    <td className='td1'>Giới tính: &ensp;</td>
+                                    <td className='td2'>{this.state.thisPatient.patientGender === '1' ? 'Nam' : 'Nữ'}</td>
+                                </tr>
+                                <tr><td><u><i><b>Lý do khám: </b></i></u></td></tr>
+                            </table>
+                            <textarea cols='90' rows="5" placeholder='' value={this.state.thisPatient.reason} readOnly></textarea>
+                        </div>
+                    </div>
+                </Modal>
                 <div className='nofi'>
                     <div style={{ backgroundColor: '#d3ffd3' }}>
                         <a class="notification">
@@ -102,7 +162,7 @@ class UserManage extends Component {
                             />
                         </div>
                         <div className='datepicked'>
-                            <h4>Ngày được chọn: {this.state.datePicked.getDate()}/
+                            <h4>Ngày được chọn: {this.themSo_0(this.state.datePicked.getDate())}/
                                 {+this.state.datePicked.getMonth() + 1}/
                                 {this.state.datePicked.getFullYear()}</h4>
                         </div>
@@ -116,13 +176,26 @@ class UserManage extends Component {
                             return (<div className='child'>
                                 <div className='ngaygio'>
                                     <h4>{order.clockTime ? order.clockTime : ''}</h4>
-                                    <h6>9999-99-99</h6>
-                                    <small>Đặt lúc: 9999/99/99 99:99:99</small>
+                                    <h6>
+                                        {this.themSo_0(moment(order.date)._d.getDate())}/
+                                        {+moment(order.date)._d.getMonth() + 1}/
+                                        {moment(order.date)._d.getFullYear()}
+                                    </h6>
+                                    <small>Đặt lúc: {this.themSo_0(moment(order.createdAt)._d.getHours())}:
+                                        {this.themSo_0(moment(order.createdAt)._d.getMinutes())} &nbsp;
+                                        {this.themSo_0(moment(order.createdAt)._d.getDate())}/
+                                        {+moment(order.createdAt)._d.getMonth() + 1}/
+                                        {moment(order.createdAt)._d.getFullYear()}</small>
                                 </div>
                                 <div style={{ border: '1px solid gainsboro', margin: '10px 0px 10px 0px' }}></div>
                                 <div className='infobenhnhan'>
-                                    <h5>{order.patientName ? order.patientName : ''}</h5>
-                                    <h6>{order.patientGender === 1 ? 'Nam' : 'Nữ'} - {moment(order.patientBirthday)._d.getFullYear()}</h6>
+                                    <h5 onClick={() => {
+                                        this.setState({
+                                            isOpenDetailPatient: true,
+                                            thisPatient: order
+                                        })
+                                    }}><b>{order.patientName ? order.patientName : ''}</b></h5>
+                                    <h6><b>{order.patientGender === 1 ? 'Nam' : 'Nữ'} - {moment(order.patientBirthday)._d.getFullYear()}</b></h6>
                                     <h6>{order.email ? order.email : '(không có email)'} - {order.phoneNumber ? order.phoneNumber : ''}</h6>
                                 </div>
                                 <div style={{ border: '1px solid gainsboro', margin: '10px 0px 10px 0px' }}></div>
