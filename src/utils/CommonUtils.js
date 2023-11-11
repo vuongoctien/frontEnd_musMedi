58b2a818349fbfbe5dd55d1000e2c88fe91f68ca
vuongoctien <vuongoctien@gmail.com>
2023-11-11 17:04:50 +0700

    đã biết xuất Excel

--- src/utils/CommonUtils.js
+++ src/utils/CommonUtils.js
@@ -1,3 +1,5 @@
+import * as XLSX from 'xlsx/xlsx.mjs'
+
 class CommonUtils {
     static getBase64(file) {
         return new Promise((resolve, reject) => {
@@ -5,6 +7,15 @@ class CommonUtils {
             reader.readAsDataURL(file)
             reader.onload = () => resolve(reader.result)
             reader.onerror = error => reject(error)
+        })
+    }
+    static exportExcel(data, nameSheet, nameFile) {
+        return new Promise((resolve, reject) => {
+            let wb = XLSX.utils.book_new()
+            let ws = XLSX.utils.json_to_sheet(data)
+            XLSX.utils.book_append_sheet(wb, ws, nameSheet)
+            XLSX.writeFile(wb, `${nameFile}.xlsx`)
+            resolve('Xuất file thành công')
         })
     }
 }
